80c0e96de922123fb890dcc47e0a17b458061c22
Wesley Berry <wesleyberry52@gmail.com>
2019-08-02 12:49:11 -0500

    Set up reducers

--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import allReducers from './reducers';
 
-//STORE -> globalized state
-
-//ACTION Increment
-// Function that returns an object
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
-
-//REDUCER
-const counter = (state = 0; ) => {
-
-}
-//DISPATCH
+const store = createStore(allReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
